5c67623d8893de382ef88c59470e954da4577ec2
Ssalazar62 <santiago.sk08@gmail.com>
2026-04-25 10:39:23 -0500

    listo

--- contador.js
+++ contador.js
@@ -3,23 +3,25 @@ let cuenta = 0;
 function aumentar() {
     cuenta++;
     document.getElementById("numero").innerHTML = cuenta;
+    if (cuenta < 5) {
+        document.getElementById("numero").style.color = "red";
+    } else {
+        document.getElementById("numero").style.color = "blue";
+    }
 }
 
 function disminuir() {
     cuenta--;
     document.getElementById("numero").innerHTML = cuenta;
+    if (cuenta < 5) {
+        document.getElementById("numero").style.color = "red";
+    } else {
+        document.getElementById("numero").style.color = "blue";
+    }
 }
 
 function resetear() {
     cuenta = 0;
     document.getElementById("numero").innerHTML = cuenta;
-}   
-function negativo() {
-    if  (cuenta = -1){
-    document.getElementById("numero").innerHTML = cuenta;
-    document.getElementById("numero").style.color = "red";
-}else{
     document.getElementById("numero").style.color = "blue";
-}
-
-}
+}   
